aa15132b06810309052972a9272546285742f3e5
blajeunesse <34555343+blajeunesse@users.noreply.github.com>
2022-03-08 14:01:58 -0500

    Add files via upload

---
+++ src/test/java/com/blajeunesse/springboot/quotes/QuotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blajeunesse.springboot.quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
